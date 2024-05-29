deea49f823f20fc03029adc3e2cd27548bb15bb9
poisondarterz <jc.chuan.0303@gmail.com>
2024-05-30 03:06:39 +0800

    working streamlit

--- app.py
+++ app.py
@@ -6,34 +6,35 @@ from sklearn.preprocessing import StandardScaler
 # Load the trained model
 model = joblib.load('rf_model.pkl')
 
-# Create a function to get user input
 def get_user_input():
     # Create user input fields
-    age = st.sidebar.slider('Age', 20, 100, 50)
-    sex = st.sidebar.selectbox('Sex', ['Male', 'Female'])
-    chest_pain_type = st.sidebar.selectbox('Chest Pain Type', [0, 1, 2, 3, 4])
-    resting_bp = st.sidebar.slider('Resting Blood Pressure', 80, 200)
-    cholesterol = st.sidebar.slider('Cholesterol', 100, 600)
-    fasting_blood_sugar = st.sidebar.selectbox('Fasting Blood Sugar > 120 mg/dl', [0, 1])
-    resting_ecg = st.sidebar.selectbox('Resting Electrocardiographic Results', [0, 1, 2])
-    max_heart_rate = st.sidebar.slider('Max Heart Rate Achieved', 60, 220)
-    exercise_angina = st.sidebar.selectbox('Exercise Induced Angina', [0, 1])
-    oldpeak = st.sidebar.slider('Oldpeak', 0.0, 4.0, step=0.1)
-    st_slope = st.sidebar.selectbox('ST Slope', [1, 2, 3])
+    age = st.sidebar.slider('age', 20, 100, 50)
+    sex = st.sidebar.selectbox('sex', ['Male', 'Female'])
+    chest_pain_type = st.sidebar.selectbox('chest pain type', [0, 1, 2, 3, 4])
+    resting_bp = st.sidebar.slider('resting bp s', 80, 200)
+    cholesterol = st.sidebar.slider('cholesterol', 100, 600)
+    fasting_blood_sugar = st.sidebar.selectbox('fasting blood sugar', [0, 1])
+    resting_ecg = st.sidebar.selectbox('resting ecg', [0, 1, 2])
+    max_heart_rate = st.sidebar.slider('max heart rate', 60, 220)
+    exercise_angina = st.sidebar.selectbox('exercise angina', [0, 1])
+    oldpeak = st.sidebar.slider('oldpeak', 0.0, 4.0, step=0.1)
+    st_slope = st.sidebar.selectbox('ST slope', [1, 2, 3])
+
+    sex = 1 if sex == 'Male' else 0
 
     # Store a dictionary into a data frame
     user_data = {
-        'Age': age,
-        'Sex': sex,
-        'Chest Pain Type': chest_pain_type,
-        'Resting Blood Pressure': resting_bp,
-        'Cholesterol': cholesterol,
-        'Fasting Blood Sugar > 120 mg/dl': fasting_blood_sugar,
-        'Resting Electrocardiographic Results': resting_ecg,
-        'Max Heart Rate Achieved': max_heart_rate,
-        'Exercise Induced Angina': exercise_angina,
-        'Oldpeak': oldpeak,
-        'ST Slope': st_slope
+        'age': age,
+        'sex': sex,
+        'chest pain type': chest_pain_type,
+        'resting bp s': resting_bp,
+        'cholesterol': cholesterol,
+        'fasting blood sugar': fasting_blood_sugar,
+        'resting ecg': resting_ecg,
+        'max heart rate': max_heart_rate,
+        'exercise angina': exercise_angina,
+        'oldpeak': oldpeak,
+        'ST slope': st_slope
     }
     features = pd.DataFrame(user_data, index=[0])
     return features
@@ -42,7 +43,7 @@ def get_user_input():
 user_input = get_user_input()
 
 # Scale the user input
-scaler = StandardScaler()
+scaler = joblib.load('scaler.pkl')
 user_input_scaled = scaler.transform(user_input)
 
 # Make predictions
